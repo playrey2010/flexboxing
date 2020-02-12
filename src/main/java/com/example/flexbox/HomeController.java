b0f0b14a38c2c0d950e5c0a7bb2b13715fe1da13
playrey2010 <playrey2010@gmail.com>
2020-02-12 16:53:14 -0500

    added item properties page, still unfinished

--- src/main/java/com/example/flexbox/HomeController.java
+++ src/main/java/com/example/flexbox/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/items")
+    public String flexItems() {
+        return "items";
+    }
 }
