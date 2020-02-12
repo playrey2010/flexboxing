893918878d3b067e9bb894feee6de82119f80ca1
playrey2010 <playrey2010@gmail.com>
2020-02-12 11:00:52 -0500

    basic flexboxes

---
+++ src/main/java/com/example/flexbox/FlexboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.flexbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flexbox/FlexboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flexbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
